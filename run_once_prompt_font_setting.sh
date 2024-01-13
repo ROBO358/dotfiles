c814312db2d577bd8fcecd8ab07f948d12cff0f4
ROBO358 <18186312+ROBO358@users.noreply.github.com>
2024-01-13 22:06:33 +0900

    New:font message

---
+++ run_once_prompt_font_setting.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cat <<EOF
+==========
+Download "Droid Sans Mono for Powerline Nerd Font Complete" and set it in your terminal
+
+Ex)
+curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20C
+==========
+EOF
