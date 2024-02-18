becf6573841b7dd08118069ef2b68f55bb662dc5
TSURUDO Ryosuke <18186312+ROBO358@users.noreply.github.com>
2024-02-19 02:07:13 +0900

    Fix:auto open(focus) & close

--- dot_config/nvim/lua/plugin/config/nvim-tree.lua
+++ dot_config/nvim/lua/plugin/config/nvim-tree.lua
@@ -6,12 +6,24 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- open nvim-tree when vim starts
-local function open_nvim_tree()
-	require("nvim-tree.api").tree.open()
+local function open_nvim_tree(data)
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+  -- only files please
+  if not real_file and not no_name then
+    return
+  end
+  -- open the tree but don't focus it
+  require("nvim-tree.api").tree.toggle({ focus = false })
 end
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 require("nvim-tree").setup({
+  update_focused_file = {
+    enable = true,
+  },
 	sort = {
 		sorter = "case_sensitive",
 	},
@@ -50,4 +62,28 @@ require("nvim-tree").setup({
 			"^node_modules",
 		},
 	},
+})
+
+-- auto close
+vim.api.nvim_create_autocmd("QuitPre", {
+  callback = function()
+    local tree_wins = {}
+    local floating_wins = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match("NvimTree_") ~= nil then
+        table.insert(tree_wins, w)
+      end
+      if vim.api.nvim_win_get_config(w).relative ~= '' then
+        table.insert(floating_wins, w)
+      end
+    end
+    if 1 == #wins - #floating_wins - #tree_wins then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(tree_wins) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end
 })
