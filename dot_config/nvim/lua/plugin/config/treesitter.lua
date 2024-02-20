6dff3e939b7043fcd8d626282911c261f3a9f29e
TSURUDO Ryosuke <18186312+ROBO358@users.noreply.github.com>
2024-02-20 19:01:31 +0900

    Fix:`auto_install`

--- dot_config/nvim/lua/plugin/config/treesitter.lua
+++ dot_config/nvim/lua/plugin/config/treesitter.lua
@@ -1,6 +1,7 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = all,
 	sync_install = false,
+	auto_install = true,
 	highlight = { enable = true },
 	indent = { enable = true },
 	autotag = { enable = true },
