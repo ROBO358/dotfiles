3d4ddbd2c40558c2a28ef601d446176b191b852a
TSURUDO Ryosuke <18186312+ROBO358@users.noreply.github.com>
2024-02-20 19:20:19 +0900

    Fix:indent

--- dot_config/nvim/lua/plugin/config/nvim-tree.lua
+++ dot_config/nvim/lua/plugin/config/nvim-tree.lua
@@ -57,10 +57,6 @@ require("nvim-tree").setup({
 	filters = {
 		dotfiles = false,
 		git_ignored = false,
-		custom = {
-			"^\\.git",
-			"^node_modules",
-		},
 	},
 })
 
@@ -71,19 +67,19 @@ vim.api.nvim_create_autocmd("QuitPre", {
 		local floating_wins = {}
 		local wins = vim.api.nvim_list_wins()
 		for _, w in ipairs(wins) do
-		local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
-		if bufname:match("NvimTree_") ~= nil then
-			table.insert(tree_wins, w)
-		end
-		if vim.api.nvim_win_get_config(w).relative ~= '' then
-			table.insert(floating_wins, w)
-		end
+			local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+			if bufname:match("NvimTree_") ~= nil then
+				table.insert(tree_wins, w)
+			end
+			if vim.api.nvim_win_get_config(w).relative ~= '' then
+				table.insert(floating_wins, w)
+			end
 		end
 		if 1 == #wins - #floating_wins - #tree_wins then
-		-- Should quit, so we close all invalid windows.
-		for _, w in ipairs(tree_wins) do
-			vim.api.nvim_win_close(w, true)
-		end
+			-- Should quit, so we close all invalid windows.
+			for _, w in ipairs(tree_wins) do
+				vim.api.nvim_win_close(w, true)
+			end
 		end
 	end
 })
